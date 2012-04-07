6ef6739595d48a5a310d121bd669584e60bbea7d
aditya.satrya <aditya.satrya@fa3f148f-d57c-b40d-de61-6ade6d4d2c19>
2012-04-07 07:49:19 +0000

    Bugfix: create and close spider don't have to write status to database

--- Seeder-Fetcher/dmoz/spidermanager.py
+++ Seeder-Fetcher/dmoz/spidermanager.py
@@ -5,41 +5,9 @@ from __init__ import *
 class MySpiderManager(SpiderManager):
     def create(self, spider_name, **spider_kwargs):
         global conn
-        # Delete in case
-        cursor = conn.cursor()
-        query = """
-            DELETE FROM status
-            WHERE spider = %s
-        """
-        cursor.execute(query, (spider_name))
-
-        # Then insert
-        cursor = conn.cursor()
-        query = """
-            INSERT INTO status(spider, status, lastactive_at)
-            VALUES(%s, 1, NOW())
-        """
-        cursor.execute(query, (spider_name))
-
         return super(MySpiderManager, self).create(spider_name, **spider_kwargs)
 
     def close_spider(self, spider, reason):
         global conn
-        # Delete in case
-        cursor = conn.cursor()
-        query = """
-            DELETE FROM status
-            WHERE spider = %s
-        """
-        cursor.execute(query, (spider.name))
-
-        # Then insert
-        cursor = conn.cursor()
-        query = """
-            INSERT INTO status(spider, status, lastactive_at)
-            VALUES(%s, 0, NOW())
-        """
-        cursor.execute(query, (spider.name))
-
         return super(MySpiderManager, self).close_spider(spider, reason)
 
